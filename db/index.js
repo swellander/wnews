7776fe16e2c1f3a01fbb66927b33c503c855be2f
Sam Wellander <samwellander@gmail.com>
2018-07-20 19:40:17 -0400

    change env variable

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const pg = require('pg');
 const seed = require('./seed');
 const { Client } = pg;
-const client = new Client(process.env.DATA_BASE_URL);
+const client = new Client(process.env.DATABASE_URL);
 
 client.connect();
 
